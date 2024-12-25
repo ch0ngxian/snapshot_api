8fed2ccef6f9e26ec41bee468d88ea81d95fadcf
Chong Xian <chongxian@easystore.co>
2024-12-25 23:12:35 +0800

    remove static file generations

--- snapshot_api/settings.py
+++ snapshot_api/settings.py
@@ -118,9 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
